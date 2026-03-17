8292709a293b2ceacbe96a5b82cbead77e718dcf
marijustechin <m.smiginas@gmail.com>
2026-03-17 08:03:33 +0200

    feat: add register form with zod validation, RHF and i18n support

--- web/src/shared/config/i18n/dictionary.ts
+++ web/src/shared/config/i18n/dictionary.ts
@@ -1,5 +1,10 @@
 import ltPublic from './dictionaries/lt/public.json';
+import ltForms from './dictionaries/lt/forms.json';
 
 export type Dictionary = {
   public: typeof ltPublic;
+  forms: typeof ltForms;
 };
+
+export type PublicDictionary = Dictionary['public'];
+export type FormsDictionary = Dictionary['forms'];
